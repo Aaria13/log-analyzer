46bd46041530a5b8502ba15ffbb2674b7d0c7586
Dev.aa <dev.andresarias@gmail.com>
2026-07-04 17:30:51 -0500

    Implementación bloque manejo de excpeciones con try y except

--- analizador.py
+++ analizador.py
@@ -1,14 +1,22 @@
 errores = []
 
-with open("logs/servidor.log", "r") as archivo:
-    for linea in archivo:
-        if "ERROR" in linea:
-            errores.append(linea)
+try:
 
+    with open("logs/servidor.log", "r") as archivo:
+        for linea in archivo:
+            if "ERROR" in linea:
+                errores.append(linea)
+    print("lectura exitosa")            
 
-with open("reportes/errores.txt", "w")as informe:
-    for error in errores:
+    with open("reportes/errores.txt", "w")as informe:
+        for error in errores:
             informe.write(error)
+    print("informe generado")        
+
+except FileNotFoundError as inexixtente:
+        print("Archivo no existe: ", inexixtente)
+
+
             
 
 
